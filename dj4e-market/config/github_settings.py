a2179f5aad8d16dcab5e9431b8d6c956bd7f9918
Luiz Felipe Buaszczyk <luiz.felipe@wisedoc.com.br>
2026-01-29 13:40:02 -0300

    github login

---
+++ dj4e-market/config/github_settings.py
@@ -0,0 +1,13 @@
+
+SOCIAL_AUTH_GITHUB_KEY = '224642424242424230ee'
+SOCIAL_AUTH_GITHUB_SECRET = 'f1afce7ffa5424242424242424242412af40ec57'
+
+# Ask for the user's email (don't edit this line)
+SOCIAL_AUTH_GITHUB_SCOPE = ['user:email']
+
+# Application name: ChuckList Local
+# Homepage Url: http://localhost:8000
+# Application Description: Whatever
+# Authorization callback URL: http://127.0.0.1:8000/oauth/complete/github/
+
+
